c6839be630412d206c6f1dd99d5c46768ec3c52c
JuanIgnacioGonzalez <juanignaciogonzalez.6c@gmail.com>
2022-04-01 09:57:02 -0300

    hasta el 2

--- Ejercicios_1/ej_1.py
+++ Ejercicios_1/ej_1.py
@@ -1,9 +1,7 @@
-anio = int(input("que anio"))
-if (anio%100 != 0 and anio%4 == 0 or anio%400 == 0):
-    print("es biciesto")
-else:
-    print("no es biciesto")
-
-
-    
-
+ista = [1,3,5,6,7,9,12,34,67,2,4]
+print(lista)
+num = int(input("que numero"))
+try :
+    print(lista.index(num))
+except ValueError:
+    print("no se encuentra")
