9ef43ecb3d9c7039ed9bcf135e958001cba5bbc8
JuanIgnacioGonzalez <juanignaciogonzalez.6c@gmail.com>
2022-05-06 08:05:08 -0300

    guia terminada

--- practicaPOO/practica4.py
+++ practicaPOO/practica4.py
@@ -2,22 +2,13 @@ class Operacion:
     def __init__(self):
         self.n1 = int(input("numero 1"))
         self.n2= int(input("numero 2"))
+        self.operaciones()
         
-    def suma(self):
+        
+    def operaciones(self):
         print(self.n1+self.n2)
-    
-    def resta(self):
         print(self.n1-self.n2)
-
-    def mult(self):
         print(self.n1*self.n2)
-
-    def div(self):
         print(self.n1/self.n2)
 
-
 o1=Operacion()
-o1.div()
-o1.mult()
-o1.suma()
-o1.resta()
